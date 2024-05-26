ac414c99e98067dece9063588b4caabda02a4127
kk <38394196+kkwhf@users.noreply.github.com>
2024-05-26 23:23:31 +0800

    更新 Test.js

--- Test.js
+++ Test.js
@@ -6,7 +6,7 @@ hostname = otter-api.codefuture.top
 */
 if ($request.url.includes('v1/user/current')) {
     let body = JSON.parse($response.body);
-    Object.assign(body.data, { isVip: "true", vipType: "monthly", vipDeadline: "9999-09-06T06:06:06Z" });
+    Object.assign(body.data, { isVip: "true", vipType: "yearly", vipDeadline: "9999-09-06T06:06:06Z" });
     $done({ body: JSON.stringify(body) });
 } else {
     $done({});
