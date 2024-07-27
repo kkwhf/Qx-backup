7e22cfe6f55d791d4158f35f1e4aef7bbe874faf
kk <38394196+kkwhf@users.noreply.github.com>
2024-07-27 13:38:18 +0800

    更新 banAD.js

--- banAD.js
+++ banAD.js
@@ -202,13 +202,12 @@
 
 # > 彩云天气
 # hostname = *.cyapi.cn, api.caiyunapp.com
-^https?:\/\/api\.caiyunapp\.com\/v1\/activity url reject-dict
-^https:\/\/biz\.cyapi\.cn\/p\/v1\/entries\? url reject-dict
+^https:\/\/api\.caiyunapp\.com\/v1\/activity\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
+^https:\/\/biz\.cyapi\.cn\/(p\/v1\/entries|p\/v1\/trial_card\/info|v2\/product) url reject-dict
 ^https:\/\/biz\.cyapi\.cn\/(p\/v1\/vip_info|v2\/user) url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-^https:\/\/starplucker\.cyapi\.cn\/v3\/operation\/homefeatures\? url reject-dict
+^https:\/\/starplucker\.cyapi\.cn\/v3\/(config\/cypage\/\w+\/conditions|notification\/message_center|operation\/homefeatures) url reject-dict
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/(nafp\/origin_images|satellite)\? url script-request-header https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-
 # > 车来了
 # hostname = pic1.chelaile.net.cn, api.chelaile.net.cn, atrace.chelaile.net.cn, web.chelaile.net.cn, cdn.*.chelaileapp.cn
 ;^https?:\/\/pic1\.chelaile\.net\.cn\/adv\/ url reject
