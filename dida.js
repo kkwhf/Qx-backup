ceba39426fd5ad8aad41761f88abfec39aebeb5b
kk <38394196+kkwhf@users.noreply.github.com>
2024-08-11 13:18:03 +0800

    更新 dida.js

---
+++ dida.js
@@ -0,0 +1,19 @@
+/*
+
+滴答
+
+[rewrite_local]
+https://dida365.com/api/v2/user/status url script-response-body ddqd.js
+
+[mitm] 
+hostname = dida365.com
+*/
+    let obj = JSON.parse($response.body);
+    // 不能删
+    obj.username = "我是kk";  
+    obj.proEndDate = "5201-01-01T00:00:00.000+0000";  
+    obj.pro = true;
+  obj.teamPro = true;
+    $done({
+        body: JSON.stringify(obj)
+    });
