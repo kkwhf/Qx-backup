fd295a7ee1864f17e8318ed8bdea79893504d24a
kk <38394196+kkwhf@users.noreply.github.com>
2024-10-29 20:56:07 +0800

    更新 i.js

--- dida.js
+++ dida.js
@@ -9,7 +9,7 @@ https://dida365.com/api/v2/user/status url script-response-body https://raw.gith
 hostname = dida365.com
 */
     let obj = JSON.parse($response.body);
-    // 不能删
+    // 
     obj.username = "我是kk";  
     obj.proEndDate = "5201-01-01T00:00:00.000+0000";  
     obj.pro = true;
--- i.js
+++ i.js
@@ -1,19 +1,11 @@
 /*************************************
 
-项目名称：iTunes-系列解锁合集
-更新日期：2024-10-17
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-使用说明：如果脚本无效，请先排除是否脚本冲突
-特别说明：此脚本可能会导致App Store无法登录ID
-解决方法：关[MITM][脚本][代理工具]方法选一即可
-已解锁App传送门：https://too.st/iTunes
+
 
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/kkwhf/Qx-backup/refs/heads/main/i.js
 
 [mitm]
 hostname = buy.itunes.apple.com
